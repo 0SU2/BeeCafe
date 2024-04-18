4555fbb5ccc1976916c46384cd4f0b2f420b6a52
Jonathan Antonio Vallejo Perez <jonat@Jonathan.Jonathan>
2024-04-17 23:47:03 -0600

    Avance Diseño de Login

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['#FFC600', '#FFFFFF']} // Amarillo en la parte superior a blanco hacia abajo
+        style={styles.background}
+        start={{ x: 0, y: 0 }} // Inicio del gradiente en la parte superior
+        end={{ x: 0, y: 1 }}   // Final del gradiente en la parte inferior
+      />
+      <Text style={styles.titulo}>BeeCafe</Text>
+      <Text style={styles.subTitle}>Iniciar sesión en su cuenta</Text>
+      <TextInput 
+        style={styles.textInput}
+        placeholder="Correo Electronico" 
+      />
+      <TextInput 
+        style={styles.textInput}
+        placeholder="Contraseña" 
+      />
+      <View style={{ height: 20 }} //Salto de linea 
+      /> 
+      <LinearGradient
+        // Button Linear Gradient
+        colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={styles.button}>
+        <Text style={styles.text}>Iniciar Sesión</Text>
+      </LinearGradient>
     </View>
   );
 }
@@ -13,8 +37,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ffc600', // Puedes eliminar o ajustar este color de fondo si ya no es necesario
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0, // Modificado para extender el gradiente a todo el contenedor
+  },
+  button: {
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text: {
+    backgroundColor: 'transparent',
+    fontSize: 15,
+    color: '#fff',
+  },
+  titulo: {
+    fontSize: 90,
+    color: '#1b3b6a',
+    fontWeight: 'bold'
+  },
+  subTitle: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  textInput: {
+    padding: 10,
+    paddingStart: 20,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff',
+    borderColor: '#000', // Color del marco: negro
+    borderWidth: 1 // Ancho del marco
   },
 });
