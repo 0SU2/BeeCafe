8974b18fe721129fb287a77ae5f567864f3cab48
Jonathan Antonio Vallejo Perez <jonat@Jonathan.Jonathan>
2024-04-18 21:02:24 -0600

    Ajustes en el TextInput de contraseña , logo y registrarse

--- client/App.tsx
+++ client/App.tsx
@@ -1,35 +1,50 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+//import logoImage from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <LinearGradient
-        // Background Linear Gradient
-        colors={['#FFC600', '#FFFFFF']} // Amarillo en la parte superior a blanco hacia abajo
+        colors={['#FFC600', '#FFFFFF']}
         style={styles.background}
-        start={{ x: 0, y: 0 }} // Inicio del gradiente en la parte superior
-        end={{ x: 0, y: 1 }}   // Final del gradiente en la parte inferior
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
       />
       <Text style={styles.titulo}>BeeCafe</Text>
-      <Text style={styles.subTitle}>Iniciar sesión en su cuenta</Text>
+      <Image
+        source={require('./assets/LogoBeeCafe1.png')}
+        style={styles.logo}
+      />
+      <Text style={styles.subTitle}>Inicie Sesion con su Cuenta</Text>
       <TextInput 
         style={styles.textInput}
         placeholder="Correo Electronico" 
       />
       <TextInput 
         style={styles.textInput}
-        placeholder="Contraseña" 
+        placeholder="Contraseña"
+        secureTextEntry={true} 
       />
-      <View style={{ height: 20 }} //Salto de linea 
-      /> 
-      <LinearGradient
-        // Button Linear Gradient
-        colors={['#4c669f', '#3b5998', '#192f6a']}
-        style={styles.button}>
-        <Text style={styles.text}>Iniciar Sesión</Text>
-      </LinearGradient>
+      <View style={{ height: 20 }} />
+      <View style={styles.button}>
+        <LinearGradient
+          colors={['#4c669f', '#3b5998', '#192f6a']}
+          style={styles.gradient}>
+          <Button
+            onPress={() => alert('Prueba de Alerta')}
+            title="Iniciar Sesión"
+            color="#ffffff"
+          />
+        </LinearGradient>
+        <Text style={styles.textoRegistro}>
+        No tienes cuenta?   
+        <Text style={styles.linkRegistro} onPress={() => console.log('Llevar al Formulario de Registro')}>
+           Regístrate
+        </Text>
+      </Text>
+      </View>
     </View>
   );
 }
@@ -39,19 +54,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ffc600', // Puedes eliminar o ajustar este color de fondo si ya no es necesario
+    backgroundColor: '#ffc600',
   },
   background: {
     position: 'absolute',
     left: 0,
     right: 0,
     top: 0,
-    bottom: 0, // Modificado para extender el gradiente a todo el contenedor
+    bottom: 0,
   },
   button: {
-    padding: 15,
-    alignItems: 'center',
+    width: '80%',
     borderRadius: 5,
+  },
+  gradient: {
+    borderRadius: 5,
+    overflow: 'hidden', // Mantiene el gradiente dentro del borde redondeado
   },
   text: {
     backgroundColor: 'transparent',
@@ -66,6 +84,7 @@ const styles = StyleSheet.create({
   subTitle: {
     fontSize: 20,
     color: 'gray',
+    marginTop: 10,
   },
   textInput: {
     padding: 10,
@@ -75,7 +94,24 @@ const styles = StyleSheet.create({
     marginTop: 20,
     borderRadius: 30,
     backgroundColor: '#fff',
-    borderColor: '#000', // Color del marco: negro
-    borderWidth: 1 // Ancho del marco
+    borderColor: '#000',
+    borderWidth: 1
   },
+  logo: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: 5,
+  },
+  textoRegistro: {
+    color: 'black', // o cualquier otro color que estés usando
+    fontSize: 16,
+    marginTop: 20, // Ajusta el margen según necesites
+    textAlign: 'center', // Alinea el texto al centro horizontalmente
+  },
+  linkRegistro: {
+    color: 'blue',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
 });
