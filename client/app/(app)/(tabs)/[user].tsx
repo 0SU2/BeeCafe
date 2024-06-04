5c35d890c4b0b6f8db23615a1c54332c2c7a9a4e
Coronilla <e.coronillahernandez@ugto.com>
2024-06-03 22:38:38 -0600

    Lista de pedidos

--- client/app/(app)/(tabs)/[user].tsx
+++ client/app/(app)/(tabs)/[user].tsx
@@ -6,18 +6,62 @@ import { FontAwesome, MaterialIcons, Ionicons } from '@expo/vector-icons'
 import axios from 'axios'
 
 type OptionType = 'info' | 'orders' | 'logout' | null
+interface Pedido {
+  car_fecha: string;
+  men_platillo: string;
+  ped_cantidadTotal: number;
+  timestamp?: Date;
+}
 
 export default function ProfileTab() {
   const { user } = useLocalSearchParams()
   const { signOut, getUserId } = useAuth()
   const [selectedOption, setSelectedOption] = useState<OptionType>(null)
-  const [ dataPedidos, setDataPedidos ] = React.useState([]);
+  const [ dataPedidos, setDataPedidos ] = useState<Pedido[][]>([])
+  const [selectedPedido, setSelectedPedido] = useState(null)
+
+  
+
+  const processData = (pedidos: Pedido[]) : Pedido[][] => {
+    // Filtrar primero los pedidos que no tienen una fecha válida
+    pedidos = pedidos.filter(pedido => pedido.car_fecha);
+  
+    // Convertir las fechas y ordenar por fecha
+    pedidos.forEach(pedido => {
+      pedido.timestamp = new Date(pedido.car_fecha);
+    });
+    pedidos.sort((a, b) => a.timestamp!.getTime() - b.timestamp!.getTime());
+  
+    // Agrupar pedidos en un rango de 2 minutos
+    const groupedPedidos: Pedido[][] = [];
+    let tempGroup: Pedido[] = [];
+    let currentStartDate: Date | null = null;
+  
+    pedidos.forEach(pedido => {
+      if (!currentStartDate || (pedido.timestamp!.getTime() - currentStartDate.getTime()) / 60000 <= 2) {
+        tempGroup.push(pedido);
+      } else {
+        groupedPedidos.push(tempGroup);
+        tempGroup = [pedido];
+      }
+      currentStartDate = pedido.timestamp!;
+    });
+  
+    if (tempGroup.length > 0) {
+      groupedPedidos.push(tempGroup); // Asegúrate de añadir el último grupo si existe
+    }
+  
+    return groupedPedidos;
+  };
+  
 
   React.useEffect(() => {
     if(selectedOption == 'orders') {
       axios.get(`http://${process.env.EXPO_PUBLIC_IPV4_OWN}:${process.env.EXPO_PUBLIC_PORT_SERVER}/detalles/${getUserId()}` )
       .then(response => {
-        setDataPedidos(response.data.pedido)
+        const groupedData = processData(response.data.pedido)
+        setDataPedidos(groupedData)
+        setSelectedPedido(null)
       })
       .catch(error => {
         console.log(error);
@@ -25,6 +69,37 @@ export default function ProfileTab() {
     }
     
   },[selectedOption])
+
+  const handleSelectPedido = (pedido) => {
+    console.log("Pedido seleccionado: ", pedido)
+    setSelectedPedido(pedido)  // Actualiza el estado con el pedido seleccionado
+  }
+
+  const renderOrderDetails = (pedido) => {
+    const total = pedido.reduce((acc, item) => acc + parseFloat(item.ped_cantidadTotal), 0);
+
+    return(
+    <View style={styles.section}>
+      <TouchableOpacity style={styles.arrowButton} onPress={() => setSelectedPedido(null)}>
+      <Ionicons name="arrow-back" size={30} color="#007AFF" />
+    </TouchableOpacity>
+    <Text style={styles.title}>{`Detalles del Pedido`}</Text>
+    <FlatList
+      data={pedido}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({ item }) => (
+        <View style={styles.itemDetail}>
+          <Text style={styles.itemName}>{item.men_platillo}</Text>
+          <Text style={styles.itemPrice}>{`$${item.ped_cantidadTotal}`}</Text>
+        </View>
+      )}
+    />
+    <View style={styles.totalContainer}>
+        <Text style={styles.totalText}>{`Total: $${total.toFixed(2)}`}</Text>
+      </View>
+    </View>
+    )
+  }
 
   // Simulación de historial de pedidos
   const orders = [
@@ -43,17 +118,21 @@ export default function ProfileTab() {
       <TouchableOpacity style={styles.arrowButton} onPress={() => setSelectedOption(null)}>
         <Ionicons name="arrow-back" size={30} color="#007AFF" />
       </TouchableOpacity>
-      <FlatList
+      {selectedPedido ? (
+        renderOrderDetails(selectedPedido)
+      ): (
+        <FlatList
         data={dataPedidos}
-        keyExtractor={(item) => item.ped_id}
-        renderItem={({ item }) => (
-          <View style={styles.orderItem}>
-            <Text style={styles.orderText}>{item.men_platillo}</Text>
-            <Text style={styles.orderText}>{item.ped_cantidadTotal}</Text>
-            <Text style={styles.orderDate}>{item.car_fecha.substring(0,10)}</Text>
-          </View>
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity style={styles.option} onPress={() => handleSelectPedido(item)}>
+            <View style={styles.orderItem}>
+              <Text style={styles.orderText}>{`Pedido ${index + 1}`}</Text>
+            </View>
+          </TouchableOpacity>
         )}
       />
+      )}
     </>
   )
 
@@ -211,4 +290,34 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#666',
   },
+  itemDetail: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    alignItems: 'center'
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  itemPrice: {
+    fontSize: 16,
+    marginLeft: 40
+  },
+  totalContainer: {
+    marginTop: 20,
+    padding: 10,
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: '#fff',  // Fondo blanco para el contenedor del total
+    borderTopWidth: 1,        // Línea para separar la lista de ítems del total
+    borderTopColor: '#ccc'
+  },
+  totalText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#000'  // Texto en negro para destacar el total
+  }
 })
